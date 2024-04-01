58c635839b8b2ef1929987dd20f14f34e11370ca
nodauf <nodauf@users.noreply.github.com>
2024-04-01 20:10:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ class KeywordQueryEventListener(EventListener):
         text = event.get_argument() or ''
 
         if len(text) >= MIN_LEN_TO_SEARCH:
-            pids =  subprocess.Popen(f'xdotool search --onlyvisible "{text}"',
+            windows =  subprocess.Popen(f'wmctrl -lx | sed "s/  */ /g" | grep -i "{text}"',
                 shell=True,
                 stdout=subprocess.PIPE
             ).stdout.read().decode()
 
-            pid_list = pids.splitlines()
+            windows_list = windows.splitlines()
 
-            if len(pid_list) == 0:
+            if len(windows_list) == 0:
                 items.append(
                     ExtensionResultItem(
                         icon='images/not_found.png',
@@ -47,29 +47,38 @@ class KeywordQueryEventListener(EventListener):
                 )
                 return RenderResultListAction(items)
 
-            for pid in pid_list:
-                if pid:
-                    windowsname = subprocess.Popen(
-                        'xdotool getwindowname ' + pid,
-                        shell=True,
-                        stdout=subprocess.PIPE
-                    ).stdout.read().decode()
-
-                    icon_name = windowsname.split()[0]
-
+            for window in windows_list:
+                if window:
+                    windowsname = " ".join(window.split(" ")[4:])
+                    wmclass = window.split(" ")[2]
+                    #windowsname = subprocess.Popen(
+                    #    'xdotool getwindowname ' + window,
+                    #    shell=True,
+                    #    stdout=subprocess.PIPE
+                    #).stdout.read().decode()
+                    icon_name = windowsname
                     icon_theme = Gtk.IconTheme.get_default()
 
                     icon_info = icon_theme.lookup_icon(icon_name, 48, 0)
 
                     try:
-                        icon_path = icon_info.get_filename()
+                        # Some known apps
+                        if "brave" in wmclass.lower().split(".")[-1]:
+                            icon_path = icon_theme.lookup_icon("brave-desktop", 48, 0).get_filename()
+                        else:
+                            icon_path = icon_info.get_filename()
                     except AttributeError:
+                        icon_theme = Gtk.IconTheme()
                         icon_path = 'images/ios-default-app-icon.png'
+                        for icon in icon_theme.list_icons():
+                            if wmclass.lower().split(".")[-1] in icon:
+                                icon_path = icon_theme.lookup_icon(icon, 48, 0).get_filename()
+                                break
 
                     items.append(ExtensionResultItem(
                         icon=icon_path,
                         name='Open ' + windowsname,
-                        on_enter=ExtensionCustomAction(pid, keep_app_open=True)
+                        on_enter=ExtensionCustomAction(wmclass, keep_app_open=True)
                     ))
             return RenderResultListAction(items)
 
@@ -85,7 +94,7 @@ class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
         pid = event.get_data() or ""
 
-        os.system('xdotool windowactivate ' + pid)
+        os.system('wmctrl -x -a  ' + pid)
 
 
 if __name__ == '__main__':
